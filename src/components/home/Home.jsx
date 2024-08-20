3899fdf65e434ee6f25635a59c386fcc606ff98e
Web Dev Atanu <atanushil358@gmail.com>
2024-08-21 02:55:15 +0530

    update in home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="flex-center max-h-screen min-h-fit bg-gradient-to-r uppercase from-blue-500 via-purple-500 to-pink-500  text-white">
+    <div className="flex-center h-[90vh] m-4 bg-gradient-to-r uppercase from-blue-500 via-purple-500 to-pink-500  text-white">
       customize your home page
     </div>
   )
