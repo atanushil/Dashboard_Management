54675991c5b12adeadd9fa0921f8d528a60ba422
Web Dev Atanu <atanushil358@gmail.com>
2024-08-22 02:25:33 +0530

    remove canvasjs

--- src/components/common/CustomSplineChart.jsx
+++ src/components/common/CustomSplineChart.jsx
@@ -1,44 +1,10 @@
+import { linkedin } from "../../utils";
 
-import CanvasJSReact from '@canvasjs/react-charts';
-import { linkedin } from '../../utils'; 
-
-const { CanvasJSChart } = CanvasJSReact;
-
-const CustomSplineChart = ({title, data, axisYTitle, axisYPrefix  }) => {
-
-  const options = {
-    animationEnabled: true,
-    title:{
-        text: title,
-        horizontalAlign:"center",
-        fontSize:'16',
-    },
-    axisY: {
-      title: axisYTitle || "Value", 
-      prefix: axisYPrefix || "",
-    },
-    data: data && data.length > 0 ? [
-      {
-        yValueFormatString: `${axisYPrefix}#,###`, 
-
-        type: "spline", 
-        dataPoints: data, 
-      },
-    ] : [], 
-  };
-
+export default function CustomSplineChart() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      {data && data.length > 0 ? (
-        <CanvasJSChart options={options} />
-      ) : (
-        <div className="flex flex-col items-center justify-center w-full h-full p-8">
-          <img src={linkedin} alt="No data" className="w-40 h-40 mb-4" />
-          <h2 className="text-xl font-bold">No spline data available</h2>
-        </div>
-      )}
+    <div className="widget flex-center flex-col gap-2">
+      <div><img src={linkedin} alt="" className="max-h-44" /></div>
+      <div><p>No Graph Data Avaiable.</p></div>
     </div>
   );
-};
-
-export default CustomSplineChart;
+}
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -18,41 +18,6 @@ const Dashboard = () => {
           {PiechartData.map((item, i) => (
             <>
               <Widget heading={item.heading} text={item} key={i} />
-              {/* <div key={i} className="widget">
-                <CustomStackBarChart text={item} />
-              </div>
-              <div className="widget">
-                <CustomSplineChart
-                  title="Sales Overview"
-                  axisYTitle="Sales (in USD)"
-                  axisYPrefix="$"
-                  data={splineData}
-                />
-              </div> */}
-            </>
-          ))}
-          <AddWidget />
-        </div>
-      </div>
-      
-      <div className="px-3 pl-6 mb-4">
-        <p className="py-1 text-black text-lg font-light">Category name</p>
-        <div className="flex justify-between w-full overflow-x-auto custom-scrollbar py-2 gap-3 md:gap-2 items-center">
-          {PiechartData.map((item, i) => (
-            <>
-              {/* <Widget heading={item.heading} text={item} key={i} />
-              <div key={i} className="widget">
-                <CustomStackBarChart text={item} />
-              </div> */}
-              <div className="widget">
-                <CustomSplineChart
-                  title="Sales Overview"
-                  axisYTitle="Sales (in USD)"
-                  axisYPrefix="$"
-                  data={splineData} 
-                  key={i}
-                />
-              </div>
             </>
           ))}
           <AddWidget />
@@ -63,23 +28,24 @@ const Dashboard = () => {
         <div className="flex justify-between w-full overflow-x-auto custom-scrollbar py-2 gap-3 md:gap-2 items-center">
           {PiechartData.map((item, i) => (
             <>
-              {/* <Widget heading={item.heading} text={item} key={i} /> */}
-              {/* <div key={i} className="widget"> */}
-                <CustomStackBarChart text={item} key={i} />
-              {/* </div> */}
-              {/* <div className="widget">
-                <CustomSplineChart
-                  title="Sales Overview"
-                  axisYTitle="Sales (in USD)"
-                  axisYPrefix="$"
-                  data={splineData}
-                />
-              </div> */}
+              <CustomSplineChart text={item} key={i} />
             </>
           ))}
           <AddWidget />
         </div>
       </div>
+      <div className="px-3 pl-6 mb-4">
+        <p className="py-1 text-black text-lg font-light">Category name</p>
+        <div className="flex justify-between w-full overflow-x-auto custom-scrollbar py-2 gap-3 md:gap-2 items-center">
+          {PiechartData.map((item, i) => (
+            <>
+              <CustomStackBarChart text={item} key={i} />
+            </>
+          ))}
+          <AddWidget />
+        </div>
+      </div>
+
     </div>
   );
 };
